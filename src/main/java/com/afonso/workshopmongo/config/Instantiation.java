f01e809c80195a955665c4ca03dd869c44892771
Guilherme Afonso <ch.afonso.gui@gmail.com>
2020-12-24 10:42:20 -0300

    Projeção dos dados do autor com DTO

--- src/main/java/com/afonso/workshopmongo/config/Instantiation.java
+++ src/main/java/com/afonso/workshopmongo/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.afonso.workshopmongo.domain.Post;
 import com.afonso.workshopmongo.domain.User;
+import com.afonso.workshopmongo.dto.AuthorDTO;
 import com.afonso.workshopmongo.repository.PostRepository;
 import com.afonso.workshopmongo.repository.UserRepository;
 
@@ -17,27 +18,30 @@ import com.afonso.workshopmongo.repository.UserRepository;
 public class Instantiation implements CommandLineRunner {
 	
 	@Autowired
-	private UserRepository userrepository;
+	private UserRepository userRepository;
 	
 	@Autowired
-	private PostRepository postpository;
+	private PostRepository postRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
 		
-		userrepository.deleteAll();
+		userRepository.deleteAll();
+		postRepository.deleteAll();
 		
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
-
-		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem","Vou viajar para São Paulo. Abraços!", maria);
-		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia","Acordei Feliz Hoje", maria);
 		
-		userrepository.saveAll(Arrays.asList(maria,alex,bob));
-		postpository.saveAll(Arrays.asList(post1,post2));
+		userRepository.saveAll(Arrays.asList(maria,alex,bob));
+		
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem","Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia","Acordei Feliz Hoje", new AuthorDTO(maria));
+		
+		
+		postRepository.saveAll(Arrays.asList(post1,post2));
 		
 		
 	}
