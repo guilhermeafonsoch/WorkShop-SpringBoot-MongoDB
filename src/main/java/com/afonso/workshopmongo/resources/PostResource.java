e1659f8f32527b5f153c66218c9948989a27a1f2
Guilherme Afonso <ch.afonso.gui@gmail.com>
2020-12-24 11:09:38 -0300

    Acrescentando comentários aos posts

--- src/main/java/com/afonso/workshopmongo/resources/PostResource.java
+++ src/main/java/com/afonso/workshopmongo/resources/PostResource.java
@@ -1,21 +1,13 @@
 package com.afonso.workshopmongo.resources;
 
-import java.net.URI;
-import java.util.List;
-import java.util.stream.Collectors;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.afonso.workshopmongo.domain.Post;
-import com.afonso.workshopmongo.domain.User;
-import com.afonso.workshopmongo.dto.UserDTO;
 import com.afonso.workshopmongo.services.PostService;
 
 
